cecd014f3ba3fdf5b5f8a23084c6dcfe9bfb24fb
Matt Crowder <matt.crowder@walmart.com>
2018-03-07 06:41:11 -0500

    less and less

--- localdev/app.jsx
+++ localdev/app.jsx
@@ -2,19 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "babel-polyfill";
 
-import Provider from "../src/Provider";
 
 import RootComponent from "./components/index";
-import {createStore} from "../src/create-store";
-import {initialState} from "./reducers/initial-state";
-import {rootReducer} from "./reducers";
 
-const store = createStore(rootReducer, initialState);
 const App = () => (
     <div>
-        <Provider store={store}>
-            <RootComponent/>
-        </Provider>
+        <RootComponent/>
     </div>
 );
 
